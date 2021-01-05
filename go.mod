239cb5055a28ac80e0f26500e03ed5fb0921bcab
m-shaka <buddha0818@gmail.com>
2021-01-06 07:58:46 +0900

    translate

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/m-shaka/brainfuck-jit-in-go
+module github.com/m-shaka/brainfuck-jit
 
 go 1.15
+
+require github.com/m-shaka/brainfuck-jit-in-go v0.0.0-20210104135742-5d6741f6a436 // indirect
--- main.go
+++ main.go
@@ -3,11 +3,31 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/m-shaka/brainfuck-jit/internal/stack"
 	"os"
 	"strings"
 )
 
 const memorySize = 30000
+
+type bfOpKind int
+
+const (
+	invalidOp bfOpKind = iota
+	incPtr
+	decPtr
+	incData
+	decData
+	readStdin
+	writeStdout
+	jumpIfDataZero
+	jumpIfDataNotZero
+)
+
+type bfOp struct {
+	kind     bfOpKind
+	argument int
+}
 
 type program struct {
 	instructions []rune
@@ -30,6 +50,64 @@ func parse(filename string) program {
 		}
 	}
 	return program{insts}
+}
+
+func translate(p program) []bfOp {
+	pc := 0
+	programSize := len(p.instructions)
+	loopStack := stack.NewStack()
+	var ops []bfOp
+	for pc < programSize {
+		instruction := p.instructions[pc]
+		switch instruction {
+		case '[':
+			loopStack.Push(len(ops))
+			ops = append(ops, bfOp{jumpIfDataZero, 0})
+			pc++
+			break
+		case ']':
+			offset, err := loopStack.Pop()
+			if err != nil {
+				panic(fmt.Sprintf("unmatched closing ']' at pc=%d", pc))
+			}
+			ops[offset].argument = len(ops)
+			ops = append(ops, bfOp{jumpIfDataNotZero, offset})
+			pc++
+			break
+		default:
+			start := pc
+			pc++
+			for pc < programSize && p.instructions[pc] == instruction {
+				pc++
+			}
+			numRepeats := pc - start
+			var kind bfOpKind
+			switch instruction {
+			case '>':
+				kind = incPtr
+				break
+			case '<':
+				kind = decPtr
+				break
+			case '+':
+				kind = incData
+				break
+			case '-':
+				kind = decData
+				break
+			case ',':
+				kind = readStdin
+				break
+			case '.':
+				kind = writeStdout
+				break
+			default:
+				panic(fmt.Sprintf("bad char '%c' at pc=%d", instruction, pc))
+			}
+			ops = append(ops, bfOp{kind, numRepeats})
+		}
+	}
+	return ops
 }
 
 func interpret(p program, jumptable []int) {
